2dc43aa1c53925d3eb4bff9bff189f394da9e82c
kamupo <karlosmunozpons@gmail.com>
2025-08-19 15:44:37 +0200

    ­ƒÜÇ Deploy autom├ítico 19/08/2025 15:44:37,85

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "0da43cdf4e7906c6a72fddda279b615f",
+"flutter_bootstrap.js": "f7448efff3d0aa01e65b108c802d4c71",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
